9d93a4490191b6dfd7f31059cab4ad6397a24388
gclin <t96994024@gs.ncku.edu.tw>
2026-05-31 16:00:45 +0800

    CH1-L4: REPL

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for true {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
 }
