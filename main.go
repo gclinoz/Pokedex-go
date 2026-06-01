0ae049bf8d3acf2185064a66a963159455a72ffd
gclinoz <t96994024@gs.ncku.edu.tw>
2026-06-01 10:10:44 +0800

    CH1-L5 Help and Exit

--- main.go
+++ main.go
@@ -8,11 +8,55 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 
-	for true {
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		input := scanner.Text()
+		if c, exists := commands[input]; exists {
+			c.callback()
+		} else {
+			fmt.Println("Unknown command:", input)
+		}
 		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", input[0])
 	}
+}
+
+type cliCommand struct {
+	name	string
+	description	string
+	callback	func() error
+}
+
+func getCommands() map[string]cliCommand {		
+	return map[string]cliCommand{
+		"exit": {
+			name:	"exit",
+			description:	"Exit the Pokedex",
+			callback:	commandExit,
+		},
+		"help": {
+			name:	"help",
+			description:	"Displays a help message",
+			callback:	helpMsg,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func helpMsg() error {
+	commands := getCommands()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+
+	for k, v := range commands {
+		fmt.Printf("%s: %s\n", k, v.description)
+	}
+
+	return nil
 }
